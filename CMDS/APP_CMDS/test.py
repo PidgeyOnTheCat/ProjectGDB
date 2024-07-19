8e6ce09da9a1b38c98ecd338d92ed22b315ba6f6
PidgeyCat <razvanmuntean2008@gmail.com>
2024-07-19 23:14:50 +0200

    tried to spam efe lmfao

--- CMDS/APP_CMDS/test.py
+++ CMDS/APP_CMDS/test.py
@@ -15,16 +15,16 @@ openai.api_key = OPENAI_API_KEY
 
 class test(commands.Cog):
     def __init__(self, bot):
-        self.index = 0
-        self.printer.start()
+        self.bot = bot
 
-    def cog_unload(self):
-        self.printer.cancel()
+    @tasks.loop(seconds=0.2, count=100)
+    async def printer(self, member):
+        await member.send(f"hey baby :hot_face:")
+        print("brotha")
 
-    @tasks.loop(seconds=5.0)
-    async def printer(self):
-        print(self.index)
-        self.index += 1
+    @app_commands.command(name="caca", description="cacacaca.")
+    async def caca(self, interaction: discord.Interaction):
+        self.printer.start("pidgeycat")
 
 async def setup(bot):
     await bot.add_cog(test(bot))
